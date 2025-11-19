3faabbcd4304ea750875b688e09a18fe1b981d9d
adkood <ashutoshnegi195@gmail.com>
2025-11-20 02:09:19 +0530

    what is Reentrant locking

---
+++ locking/ReenterantLocking.java
@@ -0,0 +1,28 @@
+package locking;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReenterantLocking {
+
+    private final Lock lock = new ReentrantLock();
+    
+    public void A() {
+        lock.lock();
+        try {
+            this.B();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void B() {
+        lock.lock();
+        try {
+            this.B();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
