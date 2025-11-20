4d28911141b2243bf372b5dd82ced39e7b5aafab
adkood <ashutoshnegi195@gmail.com>
2025-11-20 17:15:41 +0530

    handling task async - completable future

---
+++ future/CompletableFutureExample.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureExample {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        // HANDLING TASK ASYNC 
+
+        // CompletableFuture<String> cf = new CompletableFuture<>();
+        // cf.complete("hello");
+
+        // CompletableFuture<Void> cf = CompletableFuture.runAsync(() -> {
+        //     System.out.println("Running... in bg");
+        // });
+
+        // CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(() -> 10);
+        // System.out.println(cf.get());
+        // cf.join();
+
+
+        // System.out.println(cf.get());
+
+    }
+
+}
