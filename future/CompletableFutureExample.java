e9dff9741c42e43181702696e47d11ddb8f4afd8
adkood <ashutoshnegi195@gmail.com>
2025-11-20 19:56:58 +0530

    Exception handling

--- future/CompletableFutureExample.java
+++ future/CompletableFutureExample.java
@@ -3,23 +3,31 @@ import java.util.concurrent.ExecutionException;
 
 public class CompletableFutureExample {
 
+    static CompletableFuture<Integer> getData(int n) {
+        return CompletableFuture.supplyAsync(() -> n);  
+    } 
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        // HANDLING TASK ASYNC 
+        // ---------------- HANDLING TASK ASYNC -----------------------------------
 
         // CompletableFuture<String> cf = new CompletableFuture<>();
         // cf.complete("hello");
 
         // CompletableFuture<Void> cf = CompletableFuture.runAsync(() -> {
-        //     System.out.println("Running... in bg");
+        // System.out.println("Running... in bg");
         // });
 
         // CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(() -> 10);
         // System.out.println(cf.get());
         // cf.join();
 
+        // ----------------- Channing ----------------------------------
 
-        // System.out.println(cf.get());
+        CompletableFuture<Void> cf = CompletableFuture.supplyAsync(() -> 10).thenCompose((n) -> getData(n/0)).thenAccept((r) -> System.out.println("Result : " + r)).exceptionally((e) -> {
+            System.out.println("Handling gracefully : " + e);
+            return null;
+        });
 
     }
 
